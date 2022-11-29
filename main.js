9e862c16517efd0485e01881541bfa8de6aa5e6a
melissawavicle <melissa.casole@wavicledata.com>
2022-11-29 17:08:05 -0500

    wip
    wip

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-import Chart from "chart.js/auto";
+$(function () {
+  $('input[name="daterange"]').daterangepicker(
+    {
+      opens: "left",
+    },
+    function (start, end, label) {
+      console.log(
+        "A new date selection was made: " +
+          start.format("YYYY-MM-DD") +
+          " to " +
+          end.format("YYYY-MM-DD")
+      );
+    }
+  );
+});
